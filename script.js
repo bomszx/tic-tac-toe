1b92055736a04086f45ed59e08cf6168554f9be8
bon nocos <bon.nocos@gmail.com>
2022-08-17 12:55:56 +0800

    added function to add markers to board

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 
-const gameBoard = (() => {
+
+/*
+    ** need to have as little to no global variables/functions
+
+
+
+*/
+
+const Gameboard = (() => {
+    const X_CLASS = 'x';
+    const C_CLASS = 'circle';
+
+    let circleTurn
+
     const cellElements = document.querySelectorAll('[data-cell]')
 
+    const handleClick = (e) => {
+        const cell = e.target;
+        const currentClass = circleTurn ? C_CLASS : X_CLASS;
+        placeMark(cell, currentClass)
+    }
+
+    // Eventlistener for each cell
     cellElements.forEach(cell => {
-        cell.addEventListener('click', function(e) {
-            console.log(e, 'PUTANGINA MO')
-        })
+        cell.addEventListener('click', handleClick, {once: true})
     })
-    return {cellElements}
+
+
+
+    const placeMark = (cell, currentClass) => {
+        cell.classList.add(currentClass)
+    }
+    return {cellElements, handleClick, placeMark}
 })();
+
