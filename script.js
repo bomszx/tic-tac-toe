4370eca3ecaae3159b15ccaca9984d451477f7fd
bon nocos <bon.nocos@gmail.com>
2022-08-17 17:54:34 +0800

    fixed null error on winningMessage

--- script.js
+++ script.js
@@ -5,57 +5,26 @@
 */
 
 const Gameboard = (() => {
-        const X_CLASS = 'x';
-        const C_CLASS = 'circle';
-        const board = document.getElementById('board');
-        const cellElements = document.querySelectorAll('[data-cell]')
-        const winningMessageTextElement = document.getElementById('winningMessage')
-        const restartButton = document.getElementById('restartButton')
+    let circleTurn
+    const X_CLASS = 'x';
+    const C_CLASS = 'circle';
+    const board = document.getElementById('board');
+    const cellElements = document.querySelectorAll('[data-cell]')
+    const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+    const winningMessageElement = document.getElementById('winningMessage')
+    const restartButton = document.getElementById('restartButton')
 
-        let circleTurn
-        const WINNING_COMB = [
-                            [0,1,2],
-                            [3,4,5],
-                            [6,7,8],
-                            [0,3,6],
-                            [1,4,7],
-                            [2,5,8],
-                            [0,4,8],
-                            [2,4,6]
-                        ]
-        const checkWin = (currentClass) => {
-            return WINNING_COMB.some(combination => {
-                return combination.every(index => {
-                    return cellElements[index].classList.contains(currentClass)
-                })
-            })
-        }
+    const WINNING_COMB = [
+                        [0,1,2],
+                        [3,4,5],
+                        [6,7,8],
+                        [0,3,6],
+                        [1,4,7],
+                        [2,5,8],
+                        [0,4,8],
+                        [2,4,6]
+                    ]
 
-        const isDraw = () => {
-            return [...cellElements].every(cell => {
-                return cell.classList.contains(X_CLASS) || cell.classList.contains(C_CLASS)
-            })
-        }
-
-        const endGame = (draw) => {
-            if(draw) {
-                winningMessageTextElement.innerText = 'Draw!'
-            } else {
-                winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
-            }
-            winningMessageTextElement.classList.add('show')
-        }
-
-        const setBoardHoverClass = () => {
-            board.classList.remove(X_CLASS)
-            board.classList.remove(C_CLASS)
-
-            if(circleTurn) {
-                board.classList.add(C_CLASS)
-            } else {
-                board.classList.add(X_CLASS)
-            }
-        }
         // Click eventhandler
         const handleClick = (e) => {
             const cell = e.target;
@@ -71,21 +40,53 @@ const Gameboard = (() => {
             }
         }
 
+        const endGame = (draw) => {
+            if(draw) {
+                winningMessageTextElement.innerText = 'Draw!'
+            } else {
+                winningMessageTextElement.innerText = `${circleTurn ? "O's win" : "X's win"} Wins!`
+            }
+            winningMessageElement.classList.add('show')
+        }
+
+        const isDraw = () => {
+            return [...cellElements].every(cell => {
+                return cell.classList.contains(X_CLASS) || cell.classList.contains(C_CLASS)
+            })
+        }
+
+        const setBoardHoverClass = () => {
+            board.classList.remove(X_CLASS)
+            board.classList.remove(C_CLASS)
+
+            if(circleTurn) {
+                board.classList.add(C_CLASS)
+            } else {
+                board.classList.add(X_CLASS)
+            }
+        }
+
+        const checkWin = (currentClass) => {
+            return WINNING_COMB.some(combination => {
+                return combination.every(index => {
+                    return cellElements[index].classList.contains(currentClass)
+                })
+            })
+        }
+
         const startGame = () => {
             circleTurn = false;
             cellElements.forEach(cell => {
-                cell.classList.remove(X_CLASS)
-                cell.classList.remove(C_CLASS)
-                cell.removeEventListener('click', handleClick)
                 cell.addEventListener('click', handleClick, {once: true})
             })
             setBoardHoverClass()
-            winningMessageTextElement.classList.remove('show')
         }
 
-        startGame();
+        restartButton.addEventListener('click', function(e) {
+            console.log('ems')
+        })
 
-        restartButton.addEventListener('click', startGame)
+        startGame();
 
         const placeMark = (cell, currentClass) => {
             cell.classList.add(currentClass)
