0f094d5e6d187d9e0bb31f8a87f5741e6491f5e9
bon nocos <bon.nocos@gmail.com>
2023-01-09 23:57:43 +0800

    added function to render arr contents to html

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-const gameBoard = (() => {
-    const board = [];
+// gameboard module
+const board = (() => {
+    const gameBoard = ["xeeee", "2", "", "5", "x", "", "", "", "o"];
+    const cells = document.querySelectorAll('.cell')
 
-    return {board}
+    const render = () => {
+        for(let i = 0; i < cells.length; i++) {
+            cells[i].innerText = gameBoard[i]
+        }       
+    }
+    return {gameBoard, cells, render}
 })();
 
-const Player(name, marker) {
+// player factory
+const Player = (name, marker) => {
+    const writeMark = () => {
+        console.log(`my name is mark and this is my mark ${marker}`);eeeeeeeeeeeeeeeee
+    }
 
+    return {name, marker, writeMark}
 }
+
+/*
+Set up your HTML and write a JavaScript function that will render the contents of the gameboard array to the webpage (for now you can just manually fill in the array with "X"s and "O"s) 
+*/
