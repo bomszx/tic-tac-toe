a9c5486dad418559c2ef7a3f087a8903b6a20cd3
bon nocos <bon.nocos@gmail.com>
2023-02-10 15:30:21 +0800

    refactoring

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 /*
- * create a gameBoard 3x3
- * addEventListeners to each cell of the board
- *  - will call a fn that will update the board with the marker
- * need player factory for player objects (x, o) 
- */
-/*
  * You do, but generally not directly. When the DOM board is clicked, you might be able to tell which cell was clicked. And you might tell some sort of "game manager thing" what the index of the clicked cell was. And that "game manager thing"? It might ask the gameBoard array "hey, is this index able to be set to this marker?" And that gameBoard array might respond if it can or not.
 
 Developing this one is sort of "orchestrating the conversation" between the various bits.
@@ -15,86 +9,67 @@ Great question! If you find you're getting a little stuck? I might suggest check
 // _ for private methods and properties
 
 /**
-    - PUTANGINANG INANG TIC-TAC-TOE
+    - TIC-TAC-TOE
+    
+    - click cell to play
+      - when a cell is clicked, player's mark will populate the cell
+      - if a cell is populated players should not be able to click it
+      - after a click, switch to player2
 
-  - initially, choose which marker we want,
-    - set the choice as the currentPlayer variable
-  
-  - make our board clickable..
-    - with each click want to return the index of the cell and the current marker for that cell
-      - we need this so we can use our _board array for the winning combination
+      - after each click we should check for a win/tie..
+        - tie happens if there's no 3 in a row and all cells are empty
+        - a win is if a player has 3 in a row
+      
+      How to check for a win?
+        - we have ids for all the cells
+          - we can use these id's together w/ markers for 3 in a row combinations
 
-    - check if clicked cell is empty
-    - after a click, switch players
+    playerFactory
+      - players
+        - player 
+            -methods, clicking  
+    
+    gameBoard module 
+      - _board
+      - renderBoard
+      // - eventlisteners for clicks
+      - check for win
+      - reset
+
+    game module
+      - start game
+      - switch players - if cell is not empty or other players turn
+    
   */
- 
 
-  const gameBoard = (() => {
-  // tie this array to the html board - this _board we can use to check for winning combs, and state (if a cell has an marker on it or not)
-  const _board = ["x", "o", "x", "x", "o", "x", "x", "o", "x"];
-
-  const cells = document.querySelectorAll(".cell"); // DOM Element
-
-  // display our _board arr to the DOM/HTML
-  const renderBoard = () => {
-    for(let i = 0; i < cells.length; i++) {
-      cells[i].innerText = _board[i];
-    }
-  }
-
-  // returns a copy of our _board
-  const getBoard = () => {
-    return {
-      ..._board
-    };
-  }
-
-
-  const setCell = (e) => {
-
-    e.target.innerText = game.player1.marker
-  }
-
-  cells.forEach(cell => cell.addEventListener('click', setCell));
-
-
-  const checkForWin = () => {
-
-  }
-
-  const resetBoard = () => {
-
-  }
+const gameBoard = (() => {
+  let board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const cells = document.querySelectorAll(".cell");
 
   return {
-    getBoard,
-    renderBoard,
-    setCell,
-  // returning cell so I can use it to add eventlisteners from the game module
+    board,
+    cells
   }
-})();
+})()
 
-// player factory
-const Player = (name,marker) => {
-  this.name = name;
-  this.marker = marker;
-
+const Player = (name, marker) => {
+  const move = (index, name, marker) => {
+    console.log(index, name, marker)
+  }
   return {
-    name, marker,
+    move
   }
 }
 
-// this module controls the flow of the game
-const game = (() => {
-  const player1 = Player("x", "x")
-  const player2 = Player("o", "o")
-  let currentPlayer = player1;
-
-
-  return {
-    player1,
-    player2
-
+const displayController = (() => {
+  const render = () => {
+    for(let i = 0; i < gameBoard.cells.length; i++) {
+      gameBoard.cells[i].innerText = gameBoard.board[i]
+    }
   }
-
+  return {
+    render
+  }
 })()
+displayController.render()
+
