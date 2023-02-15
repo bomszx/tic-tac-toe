2e4436ab3fa90b694881466868fc0774cd83694e
bon nocos <bon.nocos@gmail.com>
2023-02-15 22:37:45 +0800

    added displaycontroller module to display board elements to DOM

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const gameBoard = (() => {
-  let board = ['','','','','','','','','']
+  let _board = ['x','x','o','o','x','x','o','o','x']
 
   const getBoard = () => {
-    return [...board]
+    return [..._board]
   }
 
   return {
     getBoard,
   }
 })();
+
+const diplayController = (() =>{
+  const cells = Array.from(document.getElementsByClassName(('cell')));
+  console.log(cells)
+
+  cells.forEach((cell,i) => {
+    cell.innerText = gameBoard.getBoard()[i]
+  })
+
+})()
