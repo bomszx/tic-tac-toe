9eedf2138d187afa45081a02cf81f630c5550c8e
bon nocos <bon.nocos@gmail.com>
2023-02-16 13:11:58 +0800

    added function to display _board arr to DOM, or connected it to the DOM

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
-const gameBoard = (() => {
-  let _board = ['x','x','o','o','x','x','o','o','x']
+const Player = (mark) => {
+  this.mark = mark
 
-  const getBoard = () => {
-    return [..._board]
+  return {
+    mark
   }
+}
+
+const gameBoard = (() => {
+  let _board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+  const x = Player('x')
+  const o = Player('o')
+  const cells = Array.from(document.getElementsByClassName(('cell')));
+  
+  const getBoard = () => {
+    return {..._board}
+  }
+
+  // this bad boy handles the click on the DOM which changes the _board
+  const handeClick = (cell, i, player) => {
+    return _board[i] = player.mark
+  }
+
+  cells.forEach(cell => cell.addEventListener('click', (cell) => {
+    gameBoard.handeClick(cell, cell.target.id, o)
+    displayController.render()
+
+  }))
 
   return {
     getBoard,
+    handeClick,
+    cells,
   }
 })();
 
-const diplayController = (() =>{
-  const cells = Array.from(document.getElementsByClassName(('cell')));
-  console.log(cells)
+const displayController = (() =>{
+  const render = () => {
+    const cells = Array.from(document.getElementsByClassName(('cell')));
+    cells.forEach((cell, i) => {
+      cell.innerText = gameBoard.getBoard()[i];
+    })
 
-  cells.forEach((cell,i) => {
-    cell.innerText = gameBoard.getBoard()[i]
-  })
+  }
 
+  return {
+    render
+  }
 })()
