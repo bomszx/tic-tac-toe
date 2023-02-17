edf85816d2a92ae3500b27d6cb44fb3aa06ebd2c
bon nocos <bon.nocos@gmail.com>
2023-02-17 17:12:21 +0800

    added checkwin function

--- script.js
+++ script.js
@@ -19,15 +19,42 @@ const gameBoard = (() => {
 
   // this bad boy handles the click on the DOM which changes the _board
   const handeClick = (cell, i, player) => {
-    if(cell.target.innerText == "") {
-      _board[i] = currentPlayer.mark;
+
+    // if(cell.target.innerText == "") {
+
+      // _board[i] = currentPlayer.mark;
+      _placeMark(cell, i)
+      _checkForWin(player)
       _switchPlayers()
-    }
+    // }
   } 
+
+  const _placeMark = (cell, i) => {
+    if(cell.target.innerText == "")
+    _board[i] = currentPlayer.mark
+  }
 
   const _switchPlayers = () => {
     currentPlayer == o ? currentPlayer = x : currentPlayer = o;
   }
+
+  const _checkForWin = (player) => {
+    const winningCombinations = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ]
+    if((winningCombinations.some(combination => combination.every(index => _board[index] === player.mark)))) {
+      console.log('win')
+    } else {
+      console.log('draw')
+    }
+}
 
   cells.forEach(cell => cell.addEventListener('click', (cell) => {
     gameBoard.handeClick(cell, cell.target.id, currentPlayer)
@@ -36,6 +63,8 @@ const gameBoard = (() => {
   }))
 
   return {
+    _checkForWin, // remove this later
+    // _checkForDraw, // remove this later
     getBoard,
     handeClick,
     cells,
@@ -43,6 +72,7 @@ const gameBoard = (() => {
 })();
 
 const displayController = (() =>{
+
   const render = () => {
     gameBoard.cells.forEach((cell, i) => {
       cell.innerText = gameBoard.getBoard()[i];
