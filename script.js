087ddcf984587b7a8081f6deecd37ef570b14d2e
bon nocos <bon.nocos@gmail.com>
2023-02-19 23:53:02 +0800

    added checkfordraw fn

--- script.js
+++ script.js
@@ -19,15 +19,13 @@ const gameBoard = (() => {
 
   // this bad boy handles the click on the DOM which changes the _board
   const handeClick = (cell, i, player) => {
-
-    // if(cell.target.innerText == "") {
-
-      // _board[i] = currentPlayer.mark;
       _placeMark(cell, i)
-      _checkForWin(player)
-      _switchPlayers()
-    // }
-  } 
+      if(_checkForWin(player)) {
+        _endGame()
+      } else {
+        _switchPlayers()
+      }
+   } 
 
   const _placeMark = (cell, i) => {
     if(cell.target.innerText == "")
@@ -49,12 +47,28 @@ const gameBoard = (() => {
       [0,4,8],
       [2,4,6]
     ]
-    if((winningCombinations.some(combination => combination.every(index => _board[index] === player.mark)))) {
-      console.log('win')
-    } else {
-      console.log('draw')
-    }
-}
+
+    let win = winningCombinations
+    .some(combination => combination
+      .every(index => _board[index] === player.mark))
+
+      return win;
+  }
+
+  const _checkForDraw = () => {
+    let draw = cells.every(cell => cell.innerText == "x" || cell.innerText == "o")
+
+    return draw
+
+  }
+
+  const _endGame = () => {
+    const board = document.querySelector('.gameBoard')
+    const h2 = document.querySelector('.gameMessage')
+
+    h2.innerText = `${currentPlayer.mark} won!`
+    board.style.display = "none"
+  }
 
   cells.forEach(cell => cell.addEventListener('click', (cell) => {
     gameBoard.handeClick(cell, cell.target.id, currentPlayer)
@@ -63,8 +77,6 @@ const gameBoard = (() => {
   }))
 
   return {
-    _checkForWin, // remove this later
-    // _checkForDraw, // remove this later
     getBoard,
     handeClick,
     cells,
@@ -77,7 +89,6 @@ const displayController = (() =>{
     gameBoard.cells.forEach((cell, i) => {
       cell.innerText = gameBoard.getBoard()[i];
     })
-
   }
 
   return {
